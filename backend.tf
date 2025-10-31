eddd6358c4b1e157d23fc3e1b65e6a74d47a0d91
Amr <amrdabour24@gmail.com>
2025-10-31 15:12:35 +0200

    amr

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "terraformamr5312"
-    key          = "eks/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket         = "terraformamr5312"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
   }
 }
 
